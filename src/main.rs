f68d5e502e1e9a00922dae9da8370dacea089a18
Hiten Tandon <tandonhiten@gmail.com>
2024-04-24 01:33:49 +0530

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+use chrono::{DateTime, Utc};
+use email_address::EmailAddress;
+use rocket::{Build, Rocket};
+
+#[derive(Debug, Clone)]
+pub struct User {
+    uid: u128,
+    user_name: Box<str>,
+    mail_id: EmailAddress,
+}
+
+#[derive(Debug, Clone)]
+pub enum Content {
+    Text(Box<str>),
+    Image(Box<Path>),
+    Video(Box<Path>),
+}
+
+#[derive(Debug, Clone)]
+pub struct Message {
+    sender_uid: u128,
+    message_id: u128,
+    timestamp: DateTime<Utc>,
+    content: Content,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Chat {
+    chat_id: u128,
+    member_uids: Vec<u128>,
+    messages: Vec<Message>,
+}
+
+#[rocket::get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::launch]
+fn launch() -> Rocket<Build> {
+    rocket::build().mount("/hello", rocket::routes![index])
 }
